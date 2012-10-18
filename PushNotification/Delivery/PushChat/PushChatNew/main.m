8e0027dc4a1391c9c19d7b5952e9c205f56ef45e
Atul Mittal <atul.mittal@optimusinfo.com>
2012-10-18 17:56:22 +0530

    ipa file added

---
+++ PushNotification/Delivery/PushChat/PushChatNew/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//   PushChat
+//
+//  Created by Optimus on 10/12/12.
+//  Copyright (c) 2012 Opyimus Information. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
